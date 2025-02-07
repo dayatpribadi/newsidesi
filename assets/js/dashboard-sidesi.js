8319605f68b25392e35c5f6beaf567701dc7bc29
dayatpribadi <51143967+dayatpribadi@users.noreply.github.com>
2025-02-07 10:29:59 +0700

    update dashboard
    update dashboard

--- assets/js/dashboard-sidesi.js
+++ assets/js/dashboard-sidesi.js
@@ -26,17 +26,17 @@ if (chartPopulationBySex) {
                     labels: {
                         show: true,
                         name: {
-                            color: '#64748B'
+                            color: '#FFF'
                         },
                         value: {
                             show: true,
-                            color: '#3A4252',
+                            color: '#FFF',
                             fontSize: '28px',
                             fontWeight: '700'
                         },
                         total: {
                             show: true,
-                            color: '#64748B'
+                            color: '#FFF'
                         }
                     }
                 }
@@ -58,7 +58,7 @@ if (chartPopulationBySex) {
                 vertical: 30
             },
             labels: {
-                colors: '#64748B'
+                colors: '#FFF'
             },
             markers: {
                 size: 8,
@@ -87,13 +87,13 @@ var options = {
     ],
     chart: {
         type: "bar",
-        height: 342,
+        height: 220,
         toolbar: {
             show: false
         }
     },
     colors: [
-        "#FFF"
+        "#361E7D"
     ],
     plotOptions: {
         bar: {
@@ -102,30 +102,30 @@ var options = {
     },
     grid: {
         show: true,
-        borderColor: "#FFF"
+        borderColor: "#361E7D"
     },
     dataLabels: {
         enabled: false
     },
     xaxis: {
         categories: [
-            "Usia 0-5 Tahun",
-            "Usia 6-19 Tahun",
-            "Usia 20-60 Tahun",
-            "Usia Lebih dari 60 Tahun",
+            "0-5 Tahun",
+            "6-19 Tahun",
+            "20-60 Tahun",
+            "> 60 Tahun",
         ],
         axisTicks: {
             show: true,
-            color: '#ECEEF2'
+            color: '#361E7D'
         },
         axisBorder: {
             show: true,
-            color: '#ECEEF2'
+            color: '#361E7D'
         },
         labels: {
             show: true,
             style: {
-                colors: "#FFF",
+                colors: "#361E7D",
                 fontSize: "12px"
             }
         }
@@ -133,20 +133,212 @@ var options = {
     yaxis: {
         labels: {
             style: {
-                colors: "#FFF",
+                colors: "#361E7D",
                 fontSize: "12px"
             }
         },
         axisBorder: {
             show: true,
-            color: '#ECEEF2'
+            color: '#361E7D'
         },
         axisTicks: {
             show: true,
-            color: '#ECEEF2'
+            color: '#361E7D'
         }
     }
 };
 var chart = new ApexCharts(document.querySelector("#chartPopulationByAge"), options);
 chart.render();
 }
+
+const chartPopulationBySch = document.getElementById("chartPopulationBySch");
+if (chartPopulationBySch) {
+    var options = {
+        series: [
+            3633, 3070, 93, 251
+        ],
+        chart: {
+            height: 414,
+            type: "donut"
+        },
+        labels: [
+            "Belum Kawin", "Kawin", "Cerai Hidup", "Cerai Mati"
+        ],
+        colors: [
+            "#8ED838", "#00A7F4", "361E7D", "#F39C12"
+        ],
+        stroke: {
+            show: false
+        },
+        plotOptions: {
+            pie: {
+                expandOnClick: false,
+                donut: {
+                    size: '65%',
+                    labels: {
+                        show: true,
+                        name: {
+                            color: '#AD63F6'
+                        },
+                        value: {
+                            show: true,
+                            color: '#AD63F6',
+                            fontSize: '28px',
+                            fontWeight: '700'
+                        },
+                        total: {
+                            show: true,
+                            color: '#AD63F6'
+                        }
+                    }
+                }
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        tooltip: {
+            enabled: false
+        },
+        legend: {
+            show: false,
+            fontSize: '12px',
+            position: 'bottom',
+            horizontalAlign: 'center',
+            itemMargin: {
+                horizontal: 10,
+                vertical: 30
+            },
+            labels: {
+                colors: '#FFF'
+            },
+            markers: {
+                size: 8,
+                offsetX: -2,
+                shape: 'circle'
+            },
+            formatter: function(seriesName, opts) {
+                return seriesName + ": <strong>" + opts.w.globals.series[opts.seriesIndex] + "</strong>";
+            }
+        }
+    };
+    var chart = new ApexCharts(document.querySelector("#chartPopulationBySch"), options);
+    chart.render();
+}
+
+    // Real-Time Sales Chart
+    const chartPopulationByMerried = document.getElementById("chartPopulationByMerried");
+    if (chartPopulationByMerried) {
+        var options = {
+            series: [
+                {
+                    name: "Merried",
+                    data: [3633, 3070, 93, 251]
+                }
+            ],
+            chart: {
+                height: 275,
+                type: "bar",
+                toolbar: {
+                    show: false
+                }
+            },
+            
+            plotOptions: {
+                bar: {
+                    borderRadius: 0,
+                    horizontal: false,
+                    columnWidth: '32px',
+                    borderRadiusApplication: 'around',
+                    borderRadiusWhenStacked: 'all',
+                    dataLabels: {
+                        position: "top" // top, center, bottom
+                    },
+                    distributed: true
+                }
+            },
+            dataLabels: {
+                enabled: true,
+                formatter: function(val) {
+                    return val + "";
+                },
+                offsetY: 0,
+                style: {
+                    fontSize: "13px",
+                    colors: ["#64748B"]
+                }
+            },
+            xaxis: {
+                show: true,
+                categories: [
+                    "Belum Kawin", "Kawin", "Cerai Hidup", "Cerai Mati"
+                ],
+                axisTicks: {
+                    show: true,
+                    color: '#8ED838'
+                },
+                axisBorder: {
+                    show: true,
+                    color: '#8ED838'
+                },
+                labels: {
+                    show: true,
+                    style: {
+                        colors: "#8695AA",
+                        fontSize: "12px"
+                    }
+                }
+            },
+            colors: [
+                "#8ED838", "#00A7F4", "#361E7D", "#F39C12"
+            ],
+            yaxis: {
+                tickAmount: 15,
+                show: false,
+                max: 11,
+                min: 0,
+                labels: {
+                    formatter: (val) => {
+                        return val + ''
+                    },
+                    style: {
+                        colors: "#64748B",
+                        fontSize: "12px"
+                    }
+                },
+                axisBorder: {
+                    show: false,
+                    color: '#8ED838'
+                },
+                axisTicks: {
+                    show: false,
+                    color: '#8ED838'
+                }
+            },
+            grid: {
+                show: false,
+                borderColor: "#ECEEF2"
+            },
+            legend: {
+                show: false,
+                position: 'top',
+                fontSize: '12px',
+                horizontalAlign: 'center',
+                itemMargin: {
+                    horizontal: 8,
+                    vertical: 0
+                },
+                labels: {
+                    colors: '#64748B'
+                },
+                markers: {
+                    size: 6,
+                    offsetX: -2,
+                    offsetY: -.5,
+                    shape: 'circle'
+                }
+            }
+        };
+        var chart = new ApexCharts(document.querySelector("#chartPopulationByMerried"), options);
+        chart.render();
+    }
