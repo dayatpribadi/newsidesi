39882653fc943f71c083ee09494c8fa49663d9dc
dayatpribadi <51143967+dayatpribadi@users.noreply.github.com>
2025-02-07 10:59:10 +0700

    update dashboard
    update dashboard

--- assets/js/dashboard-sidesi.js
+++ assets/js/dashboard-sidesi.js
@@ -226,119 +226,421 @@ if (chartPopulationBySch) {
     chart.render();
 }
 
-    // Real-Time Sales Chart
-    const chartPopulationByMerried = document.getElementById("chartPopulationByMerried");
-    if (chartPopulationByMerried) {
-        var options = {
-            series: [
-                {
-                    name: "Merried",
-                    data: [3633, 3070, 93, 251]
-                }
-            ],
-            chart: {
-                height: 275,
-                type: "bar",
-                toolbar: {
-                    show: false
-                }
-            },
-            
-            plotOptions: {
-                bar: {
-                    borderRadius: 0,
-                    horizontal: false,
-                    columnWidth: '32px',
-                    borderRadiusApplication: 'around',
-                    borderRadiusWhenStacked: 'all',
-                    dataLabels: {
-                        position: "top" // top, center, bottom
-                    },
-                    distributed: true
-                }
-            },
-            dataLabels: {
-                enabled: true,
-                formatter: function(val) {
-                    return val + "";
+// Real-Time Sales Chart
+const chartPopulationByMerried = document.getElementById("chartPopulationByMerried");
+if (chartPopulationByMerried) {
+    var options = {
+        series: [
+            {
+                name: "Merried",
+                data: [3633, 3070, 93, 251]
+            }
+        ],
+        chart: {
+            height: 275,
+            type: "bar",
+            toolbar: {
+                show: false
+            }
+        },
+        
+        plotOptions: {
+            bar: {
+                borderRadius: 0,
+                horizontal: false,
+                columnWidth: '32px',
+                borderRadiusApplication: 'around',
+                borderRadiusWhenStacked: 'all',
+                dataLabels: {
+                    position: "top" // top, center, bottom
                 },
-                offsetY: 0,
-                style: {
-                    fontSize: "13px",
-                    colors: ["#64748B"]
-                }
+                distributed: true
+            }
+        },
+        dataLabels: {
+            enabled: true,
+            formatter: function(val) {
+                return val + "";
             },
-            xaxis: {
+            offsetY: 0,
+            style: {
+                fontSize: "13px",
+                colors: ["#64748B"]
+            }
+        },
+        xaxis: {
+            show: true,
+            categories: [
+                "Belum Kawin", "Kawin", "Cerai Hidup", "Cerai Mati"
+            ],
+            axisTicks: {
                 show: true,
-                categories: [
-                    "Belum Kawin", "Kawin", "Cerai Hidup", "Cerai Mati"
-                ],
-                axisTicks: {
-                    show: true,
-                    color: '#8ED838'
-                },
-                axisBorder: {
-                    show: true,
-                    color: '#8ED838'
-                },
-                labels: {
-                    show: true,
-                    style: {
-                        colors: "#8695AA",
-                        fontSize: "12px"
-                    }
-                }
+                color: '#8ED838'
             },
-            colors: [
-                "#8ED838", "#00A7F4", "#361E7D", "#F39C12"
-            ],
-            yaxis: {
-                tickAmount: 15,
-                show: false,
-                max: 11,
-                min: 0,
-                labels: {
-                    formatter: (val) => {
-                        return val + ''
-                    },
-                    style: {
-                        colors: "#64748B",
-                        fontSize: "12px"
-                    }
-                },
-                axisBorder: {
-                    show: false,
-                    color: '#8ED838'
-                },
-                axisTicks: {
-                    show: false,
-                    color: '#8ED838'
-                }
+            axisBorder: {
+                show: true,
+                color: '#8ED838'
             },
-            grid: {
-                show: false,
-                borderColor: "#ECEEF2"
-            },
-            legend: {
-                show: false,
-                position: 'top',
-                fontSize: '12px',
-                horizontalAlign: 'center',
-                itemMargin: {
-                    horizontal: 8,
-                    vertical: 0
-                },
-                labels: {
-                    colors: '#64748B'
-                },
-                markers: {
-                    size: 6,
-                    offsetX: -2,
-                    offsetY: -.5,
-                    shape: 'circle'
+            labels: {
+                show: true,
+                style: {
+                    colors: "#8695AA",
+                    fontSize: "12px"
                 }
             }
-        };
-        var chart = new ApexCharts(document.querySelector("#chartPopulationByMerried"), options);
-        chart.render();
-    }
+        },
+        colors: [
+            "#8ED838", "#00A7F4", "#361E7D", "#F39C12"
+        ],
+        yaxis: {
+            tickAmount: 15,
+            show: false,
+            max: 11,
+            min: 0,
+            labels: {
+                formatter: (val) => {
+                    return val + ''
+                },
+                style: {
+                    colors: "#64748B",
+                    fontSize: "12px"
+                }
+            },
+            axisBorder: {
+                show: false,
+                color: '#8ED838'
+            },
+            axisTicks: {
+                show: false,
+                color: '#8ED838'
+            }
+        },
+        grid: {
+            show: false,
+            borderColor: "#ECEEF2"
+        },
+        legend: {
+            show: false,
+            position: 'top',
+            fontSize: '12px',
+            horizontalAlign: 'center',
+            itemMargin: {
+                horizontal: 8,
+                vertical: 0
+            },
+            labels: {
+                colors: '#64748B'
+            },
+            markers: {
+                size: 6,
+                offsetX: -2,
+                offsetY: -.5,
+                shape: 'circle'
+            }
+        }
+    };
+    var chart = new ApexCharts(document.querySelector("#chartPopulationByMerried"), options);
+    chart.render();
+}
+
+
+
+const pieChart1 = document.getElementById("pieChart1");
+if (pieChart1) {
+    var options = {
+        series: [
+            196200, 190741
+        ],
+        chart: {
+            height: 414,
+            type: "donut"
+        },
+        labels: [
+            "Laki-Laki", "Perempuan",
+        ],
+        colors: [
+            "#90C7FF", "#AD63F6", 
+        ],
+        stroke: {
+            show: false
+        },
+        plotOptions: {
+            pie: {
+                expandOnClick: false,
+                donut: {
+                    size: '65%',
+                    labels: {
+                        show: true,
+                        name: {
+                            color: '#FFF'
+                        },
+                        value: {
+                            show: true,
+                            color: '#FFF',
+                            fontSize: '28px',
+                            fontWeight: '700'
+                        },
+                        total: {
+                            show: true,
+                            color: '#FFF'
+                        }
+                    }
+                }
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        tooltip: {
+            enabled: false
+        },
+        legend: {
+            show: false,
+            fontSize: '12px',
+            position: 'bottom',
+            horizontalAlign: 'center',
+            itemMargin: {
+                horizontal: 10,
+                vertical: 30
+            },
+            labels: {
+                colors: '#FFF'
+            },
+            markers: {
+                size: 8,
+                offsetX: -2,
+                shape: 'circle'
+            },
+            formatter: function(seriesName, opts) {
+                return seriesName + ": <strong>" + opts.w.globals.series[opts.seriesIndex] + "</strong>";
+            }
+        }
+    };
+    var chart = new ApexCharts(document.querySelector("#pieChart1"), options);
+    chart.render();
+}
+
+const pieChart2 = document.getElementById("pieChart2");
+if (pieChart2) {
+    var options = {
+        series: [
+            196200, 190741
+        ],
+        chart: {
+            height: 414,
+            type: "donut"
+        },
+        labels: [
+            "Laki-Laki", "Perempuan",
+        ],
+        colors: [
+            "#90C7FF", "#AD63F6", 
+        ],
+        stroke: {
+            show: false
+        },
+        plotOptions: {
+            pie: {
+                expandOnClick: false,
+                donut: {
+                    size: '65%',
+                    labels: {
+                        show: true,
+                        name: {
+                            color: '#FFF'
+                        },
+                        value: {
+                            show: true,
+                            color: '#FFF',
+                            fontSize: '28px',
+                            fontWeight: '700'
+                        },
+                        total: {
+                            show: true,
+                            color: '#FFF'
+                        }
+                    }
+                }
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        tooltip: {
+            enabled: false
+        },
+        legend: {
+            show: false,
+            fontSize: '12px',
+            position: 'bottom',
+            horizontalAlign: 'center',
+            itemMargin: {
+                horizontal: 10,
+                vertical: 30
+            },
+            labels: {
+                colors: '#FFF'
+            },
+            markers: {
+                size: 8,
+                offsetX: -2,
+                shape: 'circle'
+            },
+            formatter: function(seriesName, opts) {
+                return seriesName + ": <strong>" + opts.w.globals.series[opts.seriesIndex] + "</strong>";
+            }
+        }
+    };
+    var chart = new ApexCharts(document.querySelector("#pieChart2"), options);
+    chart.render();
+}
+
+const pieChart3 = document.getElementById("pieChart3");
+if (pieChart3) {
+    var options = {
+        series: [
+            196200, 190741
+        ],
+        chart: {
+            height: 414,
+            type: "donut"
+        },
+        labels: [
+            "Laki-Laki", "Perempuan",
+        ],
+        colors: [
+            "#90C7FF", "#AD63F6", 
+        ],
+        stroke: {
+            show: false
+        },
+        plotOptions: {
+            pie: {
+                expandOnClick: false,
+                donut: {
+                    size: '65%',
+                    labels: {
+                        show: true,
+                        name: {
+                            color: '#FFF'
+                        },
+                        value: {
+                            show: true,
+                            color: '#FFF',
+                            fontSize: '28px',
+                            fontWeight: '700'
+                        },
+                        total: {
+                            show: true,
+                            color: '#FFF'
+                        }
+                    }
+                }
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        tooltip: {
+            enabled: false
+        },
+        legend: {
+            show: false,
+            fontSize: '12px',
+            position: 'bottom',
+            horizontalAlign: 'center',
+            itemMargin: {
+                horizontal: 10,
+                vertical: 30
+            },
+            labels: {
+                colors: '#FFF'
+            },
+            markers: {
+                size: 8,
+                offsetX: -2,
+                shape: 'circle'
+            },
+            formatter: function(seriesName, opts) {
+                return seriesName + ": <strong>" + opts.w.globals.series[opts.seriesIndex] + "</strong>";
+            }
+        }
+    };
+    var chart = new ApexCharts(document.querySelector("#pieChart3"), options);
+    chart.render();
+}
+
+const pieChart4 = document.getElementById("pieChart4");
+if (pieChart4) {
+    var options = {
+        series: [
+            196200, 190741
+        ],
+        chart: {
+            height: 414,
+            type: "donut"
+        },
+        labels: [
+            "Laki-Laki", "Perempuan",
+        ],
+        colors: [
+            "#90C7FF", "#AD63F6", 
+        ],
+        stroke: {
+            show: false
+        },
+        plotOptions: {
+            pie: {
+                expandOnClick: false,
+                donut: {
+                    size: '65%',
+                    labels: {
+                        show: true,
+                        name: {
+                            color: '#FFF'
+                        },
+                        value: {
+                            show: true,
+                            color: '#FFF',
+                            fontSize: '28px',
+                            fontWeight: '700'
+                        },
+                        total: {
+                            show: true,
+                            color: '#FFF'
+                        }
+                    }
+                }
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        tooltip: {
+            enabled: false
+        },
+        legend: {
+            show: false,
+            fontSize: '12px',
+            position: 'bottom',
+            horizontalAlign: 'center',
+            itemMargin: {
+                horizontal: 10,
+                vertical: 30
+            },
+            labels: {
+                colors: '#FFF'
+            },
+            markers: {
+                size: 8,
+                offsetX: -2,
+                shape: 'circle'
+            },
+            formatter: function(seriesName, opts) {
+                return seriesName + ": <strong>" + opts.w.globals.series[opts.seriesIndex] + "</strong>";
+            }
+        }
+    };
+    var chart = new ApexCharts(document.querySelector("#pieChart4"), options);
+    chart.render();
+}
